5389938df58b268c736d17bb2a3727509b56bdcf
Александр Жаворонок <C0baltt@live.com>
2021-05-24 02:38:39 +0300

    Json

--- LinqSamples/AnalyticsProgram/Jobs/GithubRepositoryParserJob.cs
+++ LinqSamples/AnalyticsProgram/Jobs/GithubRepositoryParserJob.cs
@@ -1,8 +1,8 @@
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace AnalyticsProgram.Jobs
 {
     public class GithubRepositoryParserJob : BaseJob
@@ -16,11 +16,32 @@ namespace AnalyticsProgram.Jobs
             Client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
         }
 
+       
         public override async Task Execute(DateTime signalTime)
         {
             var result = await ProcessRepositories();
             Console.WriteLine(result);
         }
+
+        static async Task JsonJob(string[] args)
+        {
+            // сохранение данных
+            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                
+                await JsonSerializer.SerializeAsync<Person>(fs, tom);
+                Console.WriteLine("Data has been saved to file");
+            }
+
+            // чтение данных
+            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                Person restoredPerson = await JsonSerializer.DeserializeAsync<Person>(fs);
+                Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+            }
+        }
+
+
 
         public override async Task<bool> ShouldRun(DateTime signalTime)
         {
@@ -30,7 +51,7 @@ namespace AnalyticsProgram.Jobs
 
         private static async Task<string> ProcessRepositories()
         {
-            var stringTask = Client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+                var stringTask = Client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
             return await stringTask;
         }
     }
