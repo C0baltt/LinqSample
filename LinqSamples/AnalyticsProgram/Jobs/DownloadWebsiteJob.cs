8f3ab994df4155f7d3fd46fe87a6bd5ccc613ef3
Александр Жаворонок <C0baltt@live.com>
2021-05-25 22:18:33 +0300

    aligned with classwork

--- LinqSamples/AnalyticsProgram/Jobs/DownloadWebsiteJob.cs
+++ LinqSamples/AnalyticsProgram/Jobs/DownloadWebsiteJob.cs
@@ -1,23 +1,24 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using AnalyticsProgram.Utils;
 
 namespace AnalyticsProgram.Jobs
 {
-    public class WebSiteDownloadJob : BaseJob
+    public class DownloadWebsiteJob : BaseJob
     {
         private const string FilePath = "Stackoverflow.txt";
         private string _siteName = "https://stackoverflow.com/questions/26233/fastest-c-sharp-code-to-download-a-web-page";
 
         public DateTime StartJob { get; set; }
 
-        public WebSiteDownloadJob(string siteName, DateTime timeStart)
+        public DownloadWebsiteJob(string siteName, DateTime timeStart)
         {
             _siteName = "https://" + siteName.Replace("https://", "");
             StartJob = timeStart;
         }
 
-        public override Task Execute(DateTime signalTime)
+        public override Task Execute(DateTime signalTime, CancellationToken token)
         {
             WebsiteUtils.Download(_siteName, FilePath);
             return Task.CompletedTask;
--- LinqSamples/AnalyticsProgram/Jobs/GithubRepositoryParserJob.cs
+++ LinqSamples/AnalyticsProgram/Jobs/GithubRepositoryParserJob.cs
@@ -4,45 +4,40 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.Json;
+using JobScheduler;
+using System.Text.Json.Serialization;
+using System.Threading;
+
 namespace AnalyticsProgram.Jobs
 {
     public class GithubRepositoryParserJob : BaseJob
     {
+        private readonly IConsoleWrapper _consoleWrapper;
         private static readonly HttpClient Client = new();
 
         public GithubRepositoryParserJob()
         {
+            _consoleWrapper = new ConsoleWrapper();
             Client.DefaultRequestHeaders.Accept.Clear();
             Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             Client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
         }
-
        
-        public override async Task Execute(DateTime signalTime)
+        public override async Task Execute(DateTime signalTime, CancellationToken token)
         {
-            var result = await ProcessRepositories();
-            Console.WriteLine(result);
-        }
+            var result = await ProcessRepositories(token);
 
-        static async Task JsonJob(string[] args)
-        {
-            // чтение данных
-            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+            if (token.IsCancellationRequested)
             {
-                Person restoredPerson = await JsonSerializer.DeserializeAsync<Person>(fs);
-                Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+                _consoleWrapper.WriteLine("GithubRepositoryParserJob: Operation canceled.");
+                return;
             }
 
-            // сохранение данных
-            using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
-            {
-                
-                await JsonSerializer.SerializeAsync<Person>(fs, tom);
-                Console.WriteLine("Data has been saved to file");
-            }
+            var repos = JsonSerializer.Deserialize<Repository[]>(result);
+
+            // heavy results handling
+            _consoleWrapper.WriteLine(result);
         }
-
-
 
         public override async Task<bool> ShouldRun(DateTime signalTime)
         {
@@ -50,10 +45,28 @@ namespace AnalyticsProgram.Jobs
             return await base.ShouldRun(signalTime) && isConnected.Length > 0;
         }
 
-        private static async Task<string> ProcessRepositories()
+        private static async Task<string> ProcessRepositories(CancellationToken token)
         {
-                var stringTask = Client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+                var stringTask = Client.GetStringAsync("https://api.github.com/orgs/dotnet/repos", token);
             return await stringTask;
+        }
+
+        public class Repository
+        {
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+
+            [JsonPropertyName("node_id")]
+            public string NodeId { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("full_name")]
+            public string FullName { get; set; }
+
+            [JsonPropertyName("private")]
+            public bool IsPrivate { get; set; }
         }
     }
 }
