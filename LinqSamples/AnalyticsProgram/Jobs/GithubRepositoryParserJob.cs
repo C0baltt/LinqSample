e22341bdb3eb398dd7740f170abcb9f967e48a9a
Александр Жаворонок <C0baltt@live.com>
2021-05-25 21:47:25 +0300

    Start development JSON

--- LinqSamples/AnalyticsProgram/Jobs/GithubRepositoryParserJob.cs
+++ LinqSamples/AnalyticsProgram/Jobs/GithubRepositoryParserJob.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.Json;
 namespace AnalyticsProgram.Jobs
 {
     public class GithubRepositoryParserJob : BaseJob
